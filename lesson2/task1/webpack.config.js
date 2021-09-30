25a50ab056a8f16b606b57eeaef2cc5ea4a26af2
Iurii <yralei@gmail.com>
2021-09-30 21:05:05 +0300

    commit

--- lesson2/task1/webpack.config.js
+++ lesson2/task1/webpack.config.js
@@ -1,10 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
+    path: path.resolve(__dirname, 'review_build'),
     filename: 'bundle.js',
   },
   module: {
