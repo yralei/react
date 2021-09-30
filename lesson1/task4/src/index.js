6763a8c748d501b942536d39bfc6c3feb761e613
Iurii <yralei@gmail.com>
2021-09-30 15:05:55 +0300

    eject

--- lesson1/task4/src/index.js
+++ lesson1/task4/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.querySelector('#root');
+const element = (
+  <div className="greeting">
+    <div className="greeting__title">Hello, world!</div>
+    <div className="greeting__text">I'm learning React</div>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(element);
+ReactDOM.render(element, rootElement);
