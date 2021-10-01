33172410d4d354078219d37f84c9ccf4d6020f38
Iurii <yralei@gmail.com>
2021-10-01 13:02:12 +0300

    commit

---
+++ lesson2/task3/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+const rootElement = document.querySelector('#root');
+
+const renderSeconds = (time) => {
+  const seconds = new Date(time).getSeconds();
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+  const styles = {
+    color: textColor,
+    backgroundColor,
+  };
+  const element = (
+    <div className="seconds" style={styles}>
+      {seconds}
+    </div>
+  );
+
+  ReactDOM.render(element, rootElement);
+};
+setInterval(() => renderSeconds(new Date()), 1000);
