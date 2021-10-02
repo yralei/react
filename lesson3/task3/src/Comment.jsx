a6e3eae7efbd0e5d3b61b911338a19bb9e31c5d3
Iurii <yralei@gmail.com>
2021-10-02 12:41:31 +0300

    task3

--- lesson3/task3/src/Comment.jsx
+++ lesson3/task3/src/Comment.jsx
@@ -9,14 +9,6 @@ function Comment(props) {
   return (
     <div className="comment">
       <UserInfo user={props.user} />
-      {/* <div className="user-info">
-        <img
-          className="avatar"
-          src={props.user.avatarUrl}
-          alt={props.user.name}
-        />
-        <div className="user-info__name">{props.user.name}</div>
-      </div> */}
       <div className="comment__text">{props.text}</div>
       <div className="comment__date">{formatDate(props.date)}</div>
     </div>
