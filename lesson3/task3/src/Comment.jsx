4d1e2a5da5eff2d6ea48984a94ba8b2027dbc3ff
Iurii <yralei@gmail.com>
2021-10-02 12:43:47 +0300

    task3

--- lesson3/task3/src/Comment.jsx
+++ lesson3/task3/src/Comment.jsx
@@ -8,7 +8,7 @@ const formatDate = (date) => moment(date).format('DD MMM YYYY');
 function Comment(props) {
   return (
     <div className="comment">
-      <UserInfo user={props.user} />
+      <UserInfo user={props.author} />
       <div className="comment__text">{props.text}</div>
       <div className="comment__date">{formatDate(props.date)}</div>
     </div>
