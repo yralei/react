01cf942c34df245c43d05d30e074bced5352b721
Iurii <yralei@gmail.com>
2021-10-03 15:33:29 +0300

    commit

--- lesson3/task1/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import './search.scss';
-import SearchField from './Search.jsx';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<SearchField name="Tom" />, rootElement);
