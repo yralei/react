fa0720363056f41babd28719231e5424750f8cf5
Iurii <yralei@gmail.com>
2021-10-04 16:28:34 +0300

    commit

--- lesson3/task5/src/index.js
+++
@@ -1,3 +0,0 @@
-
-const exampDate = new Date(Fri Jan 01 1971 08:49:12 GMT+0300);
-console.log(exampDate.getMilliseconds())
---
+++ lesson3/task5/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Profile from './Profile.jsx';
+const rootElement = document.querySelector('#root');
+
+const userData = {
+  firstName: 'John',
+  lastName: 'Doe',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London',
+};
+ReactDOM.render(<Profile userData={userData} />, rootElement);
