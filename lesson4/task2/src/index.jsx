681071db93b856749e9e354c7dc39f34d57fee66
Iurii <yralei@gmail.com>
2021-10-05 20:15:23 +0300

    commit

--- lesson4/task2/src/index.jsx
+++ lesson4/task2/src/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Profile from './Profile.jsx';
+import Clock from './Clock.jsx';
+import App from './App.jsx';
+
 const rootElement = document.querySelector('#root');
 
-const userData = {
-  firstName: 'John',
-  lastName: 'Doe',
-  birthDate: '1991-01-17T11:11:11.819Z',
-  birthPlace: 'London',
-};
-ReactDOM.render(<Profile userData={userData} />, rootElement);
+const element = <App />;
+ReactDOM.render(element, rootElement);
