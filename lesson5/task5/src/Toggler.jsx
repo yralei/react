8405803062f028a9c82b5e28725c440e8ebd78af
Iurii <yralei@gmail.com>
2021-10-06 11:37:06 +0300

    commit

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
 
-class Toggler extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggleOn: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState((state) => ({
-      isToggleOn: !state.isToggleOn,
-    }));
-  }
-  render() {
-    return (
-      <div className="colors">
-        <button onClick={this.handleClick} className="toggler">
-          {this.state.isToggleOn ? 'ON' : 'OFF'}
-        </button>
-      </div>
-    );
-  }
-}
-
-export default Toggler;
-
 // class Toggler extends Component {
 //   constructor(props) {
 //     super(props);
 //     this.state = {
 //       isToggleOn: false,
 //     };
+//     this.handleClick = this.handleClick.bind(this);
 //   }
-//   handleClick = () => {
-//     this.setState({
-//       isToggleOn: !this.state.isToggleOn,
-//     });
-//   };
+//   handleClick() {
+//     this.setState((state) => ({
+//       isToggleOn: !state.isToggleOn,
+//     }));
+//   }
 //   render() {
 //     return (
-//       <button onClick={this.handleClick} className="toggler">
-//         {this.state.isToggleOn ? 'ON' : 'OFF'}
-//       </button>
+//       <div className="colors">
+//         <button onClick={this.handleClick} className="toggler">
+//           {this.state.isToggleOn ? 'ON' : 'OFF'}
+//         </button>
+//       </div>
 //     );
 //   }
 // }
 
 // export default Toggler;
+
+class Toggler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: false,
+    };
+  }
+  handleClick = () => {
+    this.setState({
+      isToggleOn: !this.state.isToggleOn,
+    });
+  };
+  render() {
+    return (
+      <button onClick={this.handleClick} className="toggler">
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+export default Toggler;
