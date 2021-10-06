62ea98b5780ce28d137dd68568a72d8f3599a6ce
Iurii <yralei@gmail.com>
2021-10-06 13:07:38 +0300

    commit

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 
+// class Toggler extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       isToggleOn: false,
+//     };
+//   }
+//   handleClick = () => {
+//     this.setState({
+//       isToggleOn: !this.state.isToggleOn,
+//     });
+//   };
+//   render() {
+//     return (
+//       <button className="toggler" onClick={this.handleClick}>
+//         {this.state.isToggleOn ? 'ON' : 'OFF'}
+//       </button>
+//     );
+//   }
+// }
+
+// export default Toggler;
+
 class Toggler extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isToggleOn: false,
-    };
-  }
-  handleClick = () => {
-    this.setState({
-      isToggleOn: !this.state.isToggleOn,
-    });
+  state = {
+    status: false,
+    switchButton: 'OFF',
   };
+
+  handleClick() {
+    this.setState({
+      status: !this.state.status,
+      switchButton: !this.state.status ? 'ON' : 'OFF',
+    });
+  }
+
   render() {
     return (
-      <button className="toggler" onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      <button onClick={() => this.handleClick()}>
+        {this.state.switchButton}
       </button>
     );
   }
 }
-
 export default Toggler;
