ab767133b6e2b38ddeb799b54bdb77eec9a989f0
Iurii <yralei@gmail.com>
2021-10-06 11:43:48 +0300

    commit

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -1,31 +1,5 @@
 import React, { Component } from 'react';
 
-// class Toggler extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isToggleOn: false,
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-//   handleClick() {
-//     this.setState((state) => ({
-//       isToggleOn: !state.isToggleOn,
-//     }));
-//   }
-//   render() {
-//     return (
-//       <div className="colors">
-//         <button onClick={this.handleClick} className="toggler">
-//           {this.state.isToggleOn ? 'ON' : 'OFF'}
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Toggler;
-
 class Toggler extends Component {
   constructor() {
     super();
