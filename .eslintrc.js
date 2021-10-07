8190d8b091f154429fd0c8405587735c072cbb14
Iurii <yralei@gmail.com>
2021-10-07 11:38:08 +0300

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
-  extends: [
-    'eslint-config-airbnb-base',
-    'prettier',
-    'plugin:react/recommended',
-  ],
-  parser: 'babel-eslint',
+  extends: ['eslint-config-airbnb-base', 'prettier', 'plugin:react/recommended'],
+  parser: '@babel/eslint-parser',
   env: {
     es6: true,
     node: true,
@@ -15,11 +11,13 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 2021,
     sourceType: 'module',
   },
   rules: {
     'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'react/prop-types': 0,
     'import/prefer-default-export': 0,
     'no-console': 0,
   },
