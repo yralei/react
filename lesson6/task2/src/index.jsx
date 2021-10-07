ff3460f4c199a1a92652aedffef813eb45b404da
Iurii <yralei@gmail.com>
2021-10-07 10:10:30 +0300

    commit

--- lesson6/task2/src/index.jsx
+++ lesson6/task2/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import ColorPicker from './ColorPicker.jsx';
+import Mailbox from './Mailbox.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<ColorPicker />, rootElement);
+ReactDOM.render(<Mailbox unreadMessages={[]} />, rootElement);
