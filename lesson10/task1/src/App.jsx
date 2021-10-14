d1fc2e1a6c81e889d02a562edd80b142dff3305e
Iurii <yralei@gmail.com>
2021-10-14 22:19:25 +0300

    l10t1

---
+++ lesson10/task1/src/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import UserProfile from './UserProfile';
+import UserMenu from './UserMenu';
+
+class Page extends Component {
+  state = {
+    userData: null,
+  };
+
+  componentDidMount() {
+    this.fetchUserData(this.props.userId);
+  }
+
+  fetchUserData = userId => {
+    const userUrl = `https://api.github.com/users/${userId}`;
+    fetch(userUrl)
+      .then(resronse => resronse.json())
+      .then(userData =>
+        this.setState({
+          userData,
+        }),
+      );
+  };
+  render() {
+    return (
+      <div className="page">
+        <header className="header">
+          <UserMenu userData={this.state.userData} />
+        </header>
+        <UserProfile userData={this.state.userData} />
+      </div>
+    );
+  }
+}
+
+export default Page;
---
+++ lesson10/task1/src/UserMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UserMenu = ({ userData }) => {
+  if (!userData) {
+    return null;
+  }
+  const { name, avatar_url } = userData;
+  return (
+    <div className="menu">
+      <span className="menu__name">{name}</span>
+      <img alt="User Avatar" src={avatar_url} className="menu__avatar" />
+    </div>
+  );
+};
+
+export default UserMenu;
