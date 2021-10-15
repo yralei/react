d42391d6642ca0cf99281719da91604986c94fda
Iurii <yralei@gmail.com>
2021-10-15 12:51:45 +0300

    l10t2

--- lesson10/task2/src/App.jsx
+++ lesson10/task2/src/App.jsx
@@ -23,7 +23,7 @@ class App extends Component {
   render() {
     return (
       <div className="page">
-        <h1 className="title">{`Hello, ${this.state.userData.firstName}`}</h1>
+        <h1 className="title">{`Hello, ${this.state.userData.firstName} ${this.state.userData.lastName}`}</h1>
         <main className="content">
           <ShoppingCart userName={this.state.userData} />
           <Profile userData={this.state.userData} handleChange={this.handleChange} />
