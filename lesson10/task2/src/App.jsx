5f840e7d51dc2338bb9da2705e66f7925b60bbdb
Iurii <yralei@gmail.com>
2021-10-15 10:34:39 +0300

    l10t2

---
+++ lesson10/task2/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ShoppingCart from './ShoppingCart.jsx';
+import Profile from './Profile.jsx';
+
+class Page extends Component {
+  state = {
+    userData: {
+      firstName: 'John',
+      lastName: 'Doe',
+    },
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      userData: {
+        ...this.state.userData,
+        [name]: value,
+      },
+    });
+  };
+
+  render() {
+    const { userData } = this.state;
+    return (
+      <div className="page">
+        <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
+        <main className="content">
+          <ShoppingCart userData={userData} />
+          <Profile userData={userData} handleChange={this.handleChange} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Page;
---
+++ lesson10/task2/src/ShoppingCart.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ProductsList from './ProductsList';
+import CartTitle from './CartTitle';
+
+class ShoppingCart extends Component {
+  state = {
+    cartItems: [
+      {
+        id: '1',
+        name: 'Iphone 11',
+        price: 999,
+      },
+      {
+        id: '2',
+        name: 'iPad Pro',
+        price: 799,
+      },
+    ],
+  };
+  render() {
+    const count = this.state.cartItems.length;
+    return (
+      <div className="column">
+        <CartTitle userName={this.props.userData.firstName} count={count} />
+        <ProductsList cartItems={this.state.cartItems} />
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
