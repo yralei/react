c954d4d053dad4a03c7aa2fed7c1970d38fa1ced
Iurii <yralei@gmail.com>
2021-10-15 19:00:28 +0300

    l10t3

--- lesson10/task3/src/index.jsx
+++ lesson10/task3/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Calculator from './Calculator';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Calculator />, rootElement);
