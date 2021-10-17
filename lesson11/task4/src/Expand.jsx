07d38f2ddeff9c0d46c166702d17aabed45acf7b
Iurii <yralei@gmail.com>
2021-10-17 22:22:42 +0300

    l11t3

---
+++ lesson11/task4/src/Expand.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class Expand extends Component {
+  state = {
+    isOpen: false,
+  };
+  expandToggler = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={this.expandToggler}>
+            {this.state.isOpen ? (
+              <FontAwesomeIcon icon={faChevronUp} />
+            ) : (
+              <FontAwesomeIcon icon={faChevronDown} />
+            )}
+          </button>
+        </div>
+        {this.state.isOpen && <div className="expand__content">{this.props.children}</div>}
+      </div>
+    );
+  }
+}
+
+Expand.propTypes = {
+  children: PropTypes.element.isRequired,
+  title: PropTypes.string,
+};
+Expand.defaultProps = {
+  title: '',
+};
+
+export default Expand;
