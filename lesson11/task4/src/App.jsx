7571241e6f1a473813dda380b312adc40309c14a
Iurii <yralei@gmail.com>
2021-10-17 22:48:18 +0300

    l11t3

--- lesson11/task4/src/App.jsx
+++ lesson11/task4/src/App.jsx
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import Expand from './Expand';
 
 class App extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toggleBtn = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
   render() {
     return (
       <div className="app">
-        <Expand title="some title">
+        <Expand title="some title" onClose={this.toggleBtn} isOpen={this.state.isOpen}>
           <p>
             Hooks are a new addition in React 16.8. They let you use state and other React features
             without writing a class.
--- lesson11/task4/src/Expand.jsx
+++ lesson11/task4/src/Expand.jsx
@@ -1,42 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class Expand extends Component {
-  state = {
-    isOpen: false,
-  };
-  expandToggler = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  };
-
-  render() {
-    return (
-      <div className="expand border">
-        <div className="expand__header">
-          <span className="expand__title">{this.props.title}</span>
-          <button className="expand__toggle-btn" onClick={this.expandToggler}>
-            {this.state.isOpen ? (
-              <FontAwesomeIcon icon={faChevronUp} />
-            ) : (
-              <FontAwesomeIcon icon={faChevronDown} />
-            )}
-          </button>
-        </div>
-        {this.state.isOpen && <div className="expand__content">{this.props.children}</div>}
+const Expand = ({ isOpen, children, title, onClose }) => {
+  return (
+    <div className="expand border">
+      <div className="expand__header">
+        <span className="expand__title">{title}</span>
+        <button className="expand__toggle-btn" onClick={onClose}>
+          {isOpen ? (
+            <FontAwesomeIcon icon={faChevronUp} />
+          ) : (
+            <FontAwesomeIcon icon={faChevronDown} />
+          )}
+        </button>
       </div>
-    );
-  }
-}
+      {isOpen && <div className="expand__content">{children}</div>}
+    </div>
+  );
+};
 
 Expand.propTypes = {
+  isOpen: PropTypes.bool,
   children: PropTypes.element.isRequired,
   title: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
 };
 Expand.defaultProps = {
+  isOpen: false,
   title: '',
 };
 
