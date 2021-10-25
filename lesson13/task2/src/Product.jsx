f6ec09f8abc6ba1ea7d8043aae396855faf0031c
Iurii <yralei@gmail.com>
2021-10-25 23:18:19 +0300

    l13/2

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 const Product = ({ match }) => {
-  return <div className="product">{`Product is a ${match.params.productId}`}</div>;
+  return <div className="product">{match.params.productId}</div>;
 };
 
 export default Product;
