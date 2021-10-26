2d505b1d3406a120eaaf10885167f1dc3066b91b
Iurii <yralei@gmail.com>
2021-10-26 14:58:13 +0300

    l14/3

--- lesson14/task3/src/Dimensions.jsx
+++ lesson14/task3/src/Dimensions.jsx
@@ -14,44 +14,12 @@ const Dimensions = () => {
       setDimensions({ width: innerWidth, height: innerHeight });
     };
     window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
   const { width, height } = demensions;
   return <div className="dimensions">{`${width}px - ${height}px`}</div>;
 };
-
-// class Dimensions extends Component {
-//   state = {
-//     width: null,
-//     height: null,
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener('resize', this.onResize);
-//     const { innerWidth, innerHeight } = window;
-//     this.setDimensions(innerWidth, innerHeight);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('resize', this.onResize);
-//   }
-
-//   onResize = e => {
-//     const { innerWidth, innerHeight } = e.target;
-//     // eslint-disable-next-line no-undef
-//     this.setDimensions(innerWidth, innerHeight);
-//   };
-
-//   setDimensions = (width, height) => {
-//     this.setState({
-//       width,
-//       height,
-//     });
-//     document.title = `${innerWidth} x ${innerHeight}`;
-//   };
-
-//   render() {
-//     return <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>;
-//   }
-// }
 
 export default Dimensions;
